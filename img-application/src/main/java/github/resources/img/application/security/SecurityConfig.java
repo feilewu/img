65f44126d5ef514c77a7a584aaa9adbd8b1a448a
NeNe-Baby <pfxuchn@gmail.com>
2022-10-07 13:27:42 +0800

    简化认证流程

--- img-application/src/main/java/github/resources/img/application/security/SecurityConfig.java
+++ img-application/src/main/java/github/resources/img/application/security/SecurityConfig.java
@@ -4,15 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
@@ -22,26 +19,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-
     @Autowired
     private AuthenticationEntryPoint authenticationEntryPoint;
 
     @Autowired
-    private AuthenticationProvider authenticationProvider;
-
-    @Autowired
-    private TokenFilter tokenFilter;
+    private TokenAuthenticationFilter tokenFilter;
 
     @Bean( name="authenticationManager")
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider);
     }
 
     @Override
---
+++ img-application/src/main/java/github/resources/img/application/security/TokenAuthenticationFilter.java
@@ -0,0 +1,58 @@
+package github.resources.img.application.security;
+
+import github.resources.img.application.security.token.TokenManager;
+import github.resources.img.application.utils.WebUtil;
+import github.resources.img.core.model.dto.Response;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author pfxuchn@gmail.com
+ * @date 2022/10/7 13:08
+ **/
+@Component
+public class TokenAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenManager tokenManager;
+
+    private final TokenFilterRequestMatcher tokenFilterRequestMatcher = new TokenFilterRequestMatcher("/**");
+
+    public TokenAuthenticationFilter() {
+        tokenFilterRequestMatcher.setPass("/api/user/login","/img/**");
+    }
+
+    @Override
+    protected boolean shouldNotFilter(@NotNull HttpServletRequest request) throws ServletException {
+        return !tokenFilterRequestMatcher.matches(request);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token= getAuthorization(request);
+        final Response checkTokenResp = tokenManager.checkToken(token);
+        if(checkTokenResp.isSuccess()){
+            final UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(checkTokenResp.getObj(), null, null);
+            SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            filterChain.doFilter(request,response);
+        }else {
+            WebUtil.replyJson(response,checkTokenResp);
+        }
+
+    }
+
+    public String getAuthorization(HttpServletRequest request) {
+        return request.getHeader("token");
+    }
+
+}
